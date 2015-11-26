bc5b5424b0851d4e613ba16e9524171bb813f69f
Thais Pulliam <thaispulliam@gmail.com>
2015-11-25 20:13:38 -0500

    finished lab

--- seeds.sql
+++ seeds.sql
@@ -1,8 +1,8 @@
 INSERT INTO locations 
 (name, nickname, address, latitude, longitude, phone_number, name_of_supervisor)
 VALUES
-("Rosendale", "Rose", "Rosendale, NY 12472", "74.0822° W", "41.8439° N", "845-658-3131", "Phose")
-("Eddyville", "Eddy", "Eddyville, NY 12401", "74.000° W", "41.5548° N", "845-802-0025", "Pheddy"),
+("Rosendale", "Rose", "Rosendale, NY 12472", "74.0822° W", "41.8439° N", "845-658-3131", "Phose"),
+("Eddyville", "Eddy", "Eddyville, NY 12401", "74.0000° W", "41.5548° N", "845-802-0025", "Pheddy"),
 ("Wappingers Falls", "Wapp", "Wappingers Falls, NY 12590", "73.9181° W", "41.5992° N", "845-838-0094", "Phingers");
 
 INSERT INTO crops 
--- server.rb
+++ server.rb
@@ -6,19 +6,41 @@ $db.type_translation = true
 module App
 	class Server < Sinatra::Base
 		get('/') do
+			sql = "SELECT * FROM locations"
+      		@locations = $db.execute(sql)
 			erb :index
 		end#get'/'
 
-		get('/:id') do
+		get('/crops/:id') do
+			@id = params[:id]
+			sql = "SELECT * FROM crops WHERE location='#{@id}'"      		
+			@crops = $db.execute(sql)
+
+			sql2 = "SELECT * FROM locations WHERE id='#{@id}'"
+      		@location = $db.execute(sql2)
 			erb :per_location
 		end#get'/'
 
 		post('/location') do
-			redirect to ('')
+			sql = "INSERT INTO locations (name) VALUES (?)"
+      		$db.execute(sql, params[:name].to_s)
+			redirect to ('/')
 		end#get'/'
 
-		post('/crop') do
-			redirect to ('')
+		post('/newcrop') do
+			sql = "INSERT INTO crops (name, location) VALUES (?, ?)"
+      		vals = [params[:name].to_s, params[:location].to_i]
+      		$db.execute(sql, vals)
+			redirect back
 		end#get'/'
+
+		post('/deletecrop/:id') do
+			sql = "DELETE FROM crops WHERE id = ?"
+	     	if params[:delete] == "yes"
+        		$db.execute(sql, params[:id])
+      		end
+			redirect back
+		end#get'/'
+
 	end #Server
 end #App
